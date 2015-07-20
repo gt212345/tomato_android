8b37a5c8f0fedaaf784c1dcc7f3795ce5eb97dc2
HR Wu <wuheiru.5203@gmail.com>
2015-07-20 17:08:53 +0800

    add preference

--- app/src/main/java/org/itri/tomato/QuickStartPreferences.java
+++ app/src/main/java/org/itri/tomato/QuickStartPreferences.java
@@ -6,4 +6,5 @@ package org.itri.tomato;
 public class QuickStartPreferences {
     public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
     public static final String REGISTRATION_COMPLETE = "registrationComplete";
+    public static final String USER_ID = "userId";
 }
