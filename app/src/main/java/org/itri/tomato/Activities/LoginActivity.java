ba6182dff16ebe36fa525eb4687a844181565a54
HR Wu <wuheiru.5203@gmail.com>
2015-07-22 17:27:47 +0800

    optimized import

--- app/src/main/java/org/itri/tomato/Activities/LoginActivity.java
+++ app/src/main/java/org/itri/tomato/Activities/LoginActivity.java
@@ -1,17 +1,13 @@
 package org.itri.tomato.Activities;
 
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,20 +15,9 @@ import android.widget.Toast;
 
 import org.itri.tomato.R;
 import org.itri.tomato.Utilities;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
-import java.util.Iterator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
